bfa3898cd687c5ec323fbf8bbb396f79065a409d
Rogério Bonfim <rogeriomateus@icloud.com>
2026-03-20 15:42:49 -0300

    prismaconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "10mb",
+        },
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "24eot89dvmkbxt6p.public.blob.vercel-storage.com", // O seu hostname específico
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "http",
+                hostname: "24eot89dvmkbxt6p.public.blob.vercel-storage.com", // O seu hostname específico
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
